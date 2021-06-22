1f7b96f673c6d3850278b3615fcfdf2c1bcac065
tk <49250442+tkr-sh@users.noreply.github.com>
2021-06-22 22:19:23 +0200

    Added Sweden and new Words/Sentences

--- lang_translation.py
+++ lang_translation.py
@@ -4,139 +4,369 @@ langues = {
         "en" : "Hello",
         "jp" : "こんにちは",
         "kr" : "안녕하세요",
-        'gr' : "γεια"
+        'gr' : "γεια",
+        "sw" : "Hej"
     },
     "play" : {
         "fr":"Jouer",
         "en":"Play",
         "jp":"遊ぶこと",
         "kr":"플레이",
-        "gr":"παίζω"
+        "gr":"παίζω",
+        "sw":"Att spela"
     },
     "option" : {
         "fr":"Option",
         "en":"Option",
         "jp":"オプション",
         "kr":"선택권",
-        "gr":"επιλογή"
+        "gr":"επιλογή",
+        "sw":"Alternativ"
     },
     "quit" : {
         "fr":"Quitter",
         "en":"Quit",
         "jp":"終了する",
         "kr":"떠나다",
-        "gr":"εγκαταλείπω"
+        "gr":"εγκαταλείπω",
+        "sw" : "lämna"
     },
     "languages" : {
         "fr":"Langues",
         "en":"Langages",
         "jp":"言語",
         "kr":"언어",
-        "gr":"Γλώσσες"
+        "gr":"Γλώσσες",
+        "sw" : "Språk"
     },
     "background-color" : {
         "fr":"Couleur du Fond",
         "en":"Background Color",
         "jp":"背景色",
         "kr":"배경색",
-        "gr":"Χρώμα του φόντου"
+        "gr":"Χρώμα του φόντου",
+        "sw" : "Bakgrundsfärg"
     },
     "black" : {
         "fr":"Noir",
         "en":"Black",
         "jp":"ブラック",
         "kr":"검정",
-        "gr":"μαύρος"
+        "gr":"μαύρος",
+        "sw" : "Svart"
     },
     "white" : {
         "fr":"Blanc",
         "en":"White",
         "jp":"白い",
         "kr":"하얀",
-        "gr":"λευκό"
+        "gr":"λευκό",
+        "sw" : "Vit"
     },
     "hex_game" : {
         "fr":"Jeu de Hex",
         "en":"Hex Game",
         "jp":"ヘックスゲーム",
         "kr":"육각 게임",
-        "gr":"Hex παιχνίδι"
+        "gr":"Hex παιχνίδι",
+        "sw":"Hex-spel"
     },
     "white_won" : {
         "fr":"Les blancs ont gagné !",
         "en":"White won !",
-        "jp":"白が勝った！",
-        "kr":"화이트가 이겼다!",
-        "gr":"Λευκό κέρδισε!"
+        "jp":"白が勝った !",
+        "kr":"화이트가 이겼다 !",
+        "gr":"Λευκό κέρδισε !",
+        "sw":"Svart vann !"
     },
     "black_won" : {
         "fr":"Les noirs ont gagné !",
         "en":"Black won !",
-        "jp":"黒が勝った",
-        "kr":"블랙이 이겼다!",
-        "gr":"Ο Μαύρος κέρδισε!"
+        "jp":"黒が勝った !",
+        "kr":"블랙이 이겼다 !",
+        "gr":"Ο Μαύρος κέρδισε !",
+        "sw":"Vit vann !"
     },
     "back" : {
         "fr":"Retour",
         "en":"Back",
         "jp":"戻る",
         "kr":"반환",
-        "gr":"πίσω"
+        "gr":"πίσω",
+        "sw":"Tillbaka"
     },
     "p1ay" : {
         "fr":"Easter Egg",
         "en":"Easter Egg",
         "jp":"イースター・エッグ",
         "kr":"이스터 에그",
-        "gr":"ΠΑΣΧΑΛΙΝΟ ΑΥΓΟ"
+        "gr":"ΠΑΣΧΑΛΙΝΟ ΑΥΓΟ",
+        "sw":"Påskägg"
     },
     "music" : {
         "fr":"Musique",
         "en":"Music",
         "jp":"音楽",
         "kr":"음악",
-        "gr":"ΜΟΥΣΙΚΗ"
+        "gr":"ΜΟΥΣΙΚΗ",
+        "sw":"Musik"
     },
     "vol_music" : {
         "fr":"Volume de la musique",
         "en":"Music volume",
         "jp":"音楽の音量",
         "kr":"음악 볼륨",
-        "gr":"Όγκος μουσικής"
+        "gr":"Όγκος μουσικής",
+        "sw":"Musikvolym"
     },
     "clic_to_change" : {
         "fr":"Cliquez pour changer la longueur du plateau",
         "en":"Click to change the lenght of the tray",
         "jp":"クリックしてテーブルトップの長さを変更します",
         "kr":"테이블 상판의 길이를 변경하려면 클릭하십시오",
-        "gr":"Κάντε κλικ για να αλλάξετε το μήκος της κορυφής του τραπεζιού"
+        "gr":"Κάντε κλικ για να αλλάξετε το μήκος της κορυφής του τραπεζιού",
+        "sw":"Klicka för att ändra längden på bordsskivan"
     },
     "tray_lenght" : {
         "fr":"Longueur du plateau",
         "en":"Tray lenght",
         "jp":"デッキ長",
         "kr":"데크 길이",
-        "gr":"Μήκος καταστρώματος"
+        "gr":"Μήκος καταστρώματος", 
+        "sw":"Däck längd"
     },
     "letsgo" : {
         "fr":"C'est parti !",
         "en":"Let's go !",
         "jp":"行こう ！",
         "kr":"가자 !",
-        "gr":"Πάμε !"
+        "gr":"Πάμε !",
+        "sw":"Nu går vi !"
+    },
+    "v" : {
+        "fr":"v 1.1.0",
+        "en":"v 1.1.0",
+        "jp":"v 1.1.0",
+        "kr":"v 1.1.0",
+        "gr":"v 1.1.0",
+        "sw":"v 1.1.0"
+    },
+    "github" : {
+        "fr":'Vous pouvez consulter le "README" du',
+        "en":'You can go to the "README" of the game on',
+        "jp":'githubでゲームの「README」にアクセスして、',
+        "kr":'github에서 게임의"README"로 이동하여',
+        "gr":'Μπορείτε να πάτε στο "README" του παιχνιδιού',
+        "sw":'Du kan läsa "README"'
+    },
+    "github2" : {
+        "fr":'jeu sur github pour connaître des tactiques',
+        "en":'the github to know some tactics',
+        "jp":'いくつかの 戦術を知ることができます',
+        "kr":'몇 가지 전술을 알 수 있습니다',
+        "gr":'στο github για να μάθετε μερικές τακτικές',
+        "sw":'i spelet på github för taktik'
+    },
+    "custom_color" : {
+        "fr":"Vous pouvez customiser",
+        "en":"You can customize",
+        "jp":"ゲームの色を",
+        "kr":"게임의 색상을 사용자 ",
+        "gr":"Μπορείτε να προσαρμόσετε ",
+        "sw":"Du kan anpassa "
+    },
+    "custom_color2" : {
+        "fr":"la couleur du jeu",
+        "en":"the color of the game",
+        "jp":"カスタマイズできます",
+        "kr":"지정할 수 있습니다.",
+        "gr":"το χρώμα του παιχνιδιού",
+        "sw":"spelets färg"
+    },
+    "flags" : {
+        "fr":"Il y a plusieurs langues à disposition",
+        "en":"There are several languages ​​available",
+        "jp":"利用可能な言語は多数あります",
+        "kr":"사용 가능한 다양한 언어가 있습니다.",
+        "gr":"Υπάρχει πληθώρα γλωσσών",
+        "sw":"Det finns flera språk tillgängliga"
+    },
+    " " : {
+        "fr":" ",
+        "en":" ",
+        "jp":" ",
+        "kr":" ",
+        "gr":" ",
+        "sw":" "
+    },
+    "mode" : {
+        "fr":"Pour l'instant,",
+        "en":"For now,",
+        "jp":"現在、",
+        "kr":"현재는 로컬 모드 만 ",
+        "gr":"Προς το παρόν, ",
+        "sw":"För närvarande "
+    },
+    "mode2" : {
+        "fr":"seul le mode local est disponible",
+        "en":"only local mode is available",
+        "jp":"ローカルモードのみが利用可能です",
+        "kr":"사용할 수 있습니다.",
+        "gr":"διατίθεται μόνο τοπική λειτουργία",
+        "sw":"är endast lokalt läge tillgängligt"
+    },
+    "local" : {
+        "fr":"Local",
+        "en":"Local",
+        "jp":"ローカル",
+        "kr":"현지",
+        "gr":"Τοπικό",
+        "sw":"Lokal"
+    },
+    "online" : {
+        "fr":"En ligne",
+        "en":"Online",
+        "jp":"オンライン",
+        "kr":"온라인",
+        "gr":"Σε σύνδεση",
+        "sw":"Uppkopplad"
+    },
+    "Computer" : {
+        "fr":"Ordinateur",
+        "en":"Computer",
+        "jp":"コンピュータ",
+        "kr":"컴퓨터",
+        "gr":"Υπολογιστής",
+        "sw":"Dator"
+    },
+    "?" : {
+        "fr":"?",
+        "en":"?",
+        "jp":"?",
+        "kr":"?",
+        "gr":"?",
+        "sw":"?"
+    },
+    "des_local" : {
+        "fr":"Jouer avec quelqu'un",
+        "en":"Playing with someone",
+        "jp":"隣の人と",
+        "kr":"옆에있는 사람과",
+        "gr":"Παίζοντας με κάποιον",
+        "sw":"Leker med någon "
+    },
+    "des_local2" : {
+        "fr":"à côté de vous physiquement",
+        "en":"next to you physically",
+        "jp":"物理的に遊ぶ",
+        "kr":"육체적으로 놀기",
+        "gr":"δίπλα σας φυσικά",
+        "sw":"bredvid dig fysiskt"
+    },
+    "des_online" : {
+        "fr":"Jouer en ligne, avec un ami",
+        "en":"Play online, with",
+        "jp":"友達やランダムな",
+        "kr":"친구 또는 무작위",
+        "gr":"Παίξτε online, με έναν",
+        "sw":"Spela online, med en vän  "
+    },
+    "des_online2" : {
+        "fr":"ou un adversaire aléatoire",
+        "en":"a friend or random opponent",
+        "jp":"対戦相手とオンラインでプレイする",
+        "kr":"상대와 온라인 플레이",
+        "gr":"φίλο ή έναν τυχαίο αντίπαλο",
+        "sw":"eller slumpmässig motståndare"
+    },
+    "des_ia" : {
+        "fr":"Jouer face à face contre l'IA.",
+        "en":"Play face to face against the AI.",
+        "jp":"AIと対面してプレイします",
+        "kr":"AI와 대면하십시오.단순한",
+        "gr":"Παίξτε πρόσωπο με πρόσωπο ενάντια",
+        "sw":"Spela ansikte mot ansikte mot AI. "
+    },
+    "des_ia2" : {
+        "fr":"Du mode simple à impossible.",
+        "en":" From simple to impossible.",
+        "jp":"単純なものから不可能なものまで",
+        "kr":"것에서 불가능한 것까지",
+        "gr":"στο AI. Από απλό σε αδύνατο.",
+        "sw":"Från enkelt till omöjligt."
+    },
+    "fr" : {
+        "fr":"Français",
+        "en":"French",
+        "jp":"フランス語",
+        "kr":"프랑스 국민",
+        "gr":"γαλλική γλώσσα",
+        "sw":"Franska"
+    },
+    "en" : {
+        "fr":"Anglais",
+        "en":"English",
+        "jp":"英語",
+        "kr":"영어",
+        "gr":"Αγγλικά",
+        "sw":"Engelsk"
+    },
+    "jp" : {
+        "fr":"Japonais",
+        "en":"Japanese",
+        "jp":"日本語",
+        "kr":"일본어",
+        "gr":"Ιαπωνικά",
+        "sw":"Japanska"
+    },
+    "kr" : {
+        "fr":"Coréen",
+        "en":"Korean",
+        "jp":"韓国語",
+        "kr":"한국어",
+        "gr":"κορεάτης",
+        "sw":"Koreanska"
+    },
+    "gr" : {
+        "fr":"Grec",
+        "en":"Greek",
+        "jp":"ギリシャ語",
+        "kr":"그리스 어",
+        "gr":"Ελληνικά",
+        "sw":"Grekisk"
+    },
+    "sw" : {
+        "fr":"Suédois",
+        "en":"Sweden",
+        "jp":"スウェーデンの",
+        "kr":"스웨덴어",
+        "gr":"Σουηδικά",
+        "sw":"Svenska"
+    },
+    "fn" : {
+        "fr":"Finlandais",
+        "en":"Finnish",
+        "jp":"フィンランド語",
+        "kr":"핀란드어",
+        "gr":"φινλανδικός",
+        "sw":"Finska"
+    },
+    "es" : {
+        "fr":"Espéranto",
+        "en":"Esperanto",
+        "jp":"エスペラント",
+        "kr":"에스페란토 말",
+        "gr":"εσπεράντο",
+        "sw":"Esperanto"
     },
     "plxay" : {
         "fr":"",
         "en":"",
         "jp":"",
         "kr":"",
-        "gr":""
+        "gr":"",
+        "sw":""
     }
-
-
     
 
 }
-
-#a = langues["bonjour"].get("jp")
-#print(a)
